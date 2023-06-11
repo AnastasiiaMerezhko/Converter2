8afc63654a13f8e6ef92e76de011b2a17997d5b6
Anastasiia <nastiamerezhko2003@gmail.com>
2023-06-11 21:31:22 +0200

    Add converting kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,9 +6,12 @@ public class Main {
 
     static double miles;
     static double input1;
+    static double kms;
+    static double input2;
 
     public static void main(String[] args) {
         convertMiles();
+        convertKms();
     }
 
     private static void convertMiles() {
@@ -20,7 +23,22 @@ public class Main {
 
         System.out.println(miles + " miles = " + input1 + " kilometers");
     }
+
+    private static void convertKms() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter kilometers: ");
+        kms = scanner.nextDouble();
+
+        input2 = convertKilometersToMiles(kms);
+
+        System.out.println(kms + " kilometers = " + input2 + " miles");
+    }
     private static double convertMilesToKilometers(double miles) {
         return miles * 1.609344;
+    }
+
+    private static double convertKilometersToMiles(double kms) {
+        return kms / 1.609344;
     }
 }
