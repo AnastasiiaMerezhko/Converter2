02c80fb47e7b9dc30e291ecdc7569733d38b5055
Anastasiia <nastiamerezhko2003@gmail.com>
2023-06-11 21:26:00 +0200

    Converting miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,26 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
+    static double miles;
+    static double input1;
+
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        convertMiles();
+    }
+
+    private static void convertMiles() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter miles: ");
+        miles = scanner.nextDouble();
+
+        input1 = convertMilesToKilometers(miles);
+
+        System.out.println(miles + " miles = " + input1 + " kilometers");
+    }
+    private static double convertMilesToKilometers(double miles) {
+        return miles * 1.609344;
     }
 }
